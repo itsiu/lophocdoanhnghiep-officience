89b5e4daa1433677cb968bdc69eb3d6f8b5714c0
Nguyễn Trọng Minh Hồng Phước <39150891+phuocntmh@users.noreply.github.com>
2019-11-09 16:04:19 +0700

    Ver. 1

---
+++ js/script.js
@@ -0,0 +1,50 @@
+var frm = $('#register-form');
+frm.submit(function (e) {
+    var recaptcha = $("#g-recaptcha-response").val();
+    if (recaptcha === "") {
+        e.preventDefault();
+        alert("Vui lòng kiểm tra biểu mẫu. Error: Please check the Recaptcha.");
+    }
+    else {
+        e.preventDefault();
+        $.ajax({
+            type: 'GET',
+            beforeSend: function(){
+                $("input").prop('disabled', true);
+                $("#submit-form").attr("disabled", true);
+            },
+            url: 'https://script.google.com/macros/s/AKfycbwaEEzyNScYdnA6D-LY2FUMCUNZ93AoLs5-DekcSL4MSrpfySQ/exec',
+            data: frm.serialize(),
+            success: function (data) {
+                $('#SuccessModal').modal('show');
+            },
+            error: function (data) {
+                $('#FailModal').modal('show');
+            },
+            complete: function(){
+                $("input").prop('disabled', false);
+                $("#submit-form").attr("disabled", false);
+                $('#register-form')[0].reset();
+            }
+        });
+    }
+});
+
+var countDownDate = new Date("Nov 18, 2019 23:59:59").getTime();
+    var x = setInterval(function() 
+    {
+      var now = new Date().getTime();
+      var distance = countDownDate - now;
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      document.getElementById("countdown").innerHTML = days + " ngày " + hours + " giờ "
+  + minutes + " phút " + seconds + " giây"; 
+      if (distance < 0) 
+      {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "ĐÃ HẾT HẠN ĐĂNG KÝ";
+        document.getElementById("submit-form").disabled = true;
+      }
+    }, 1000);
